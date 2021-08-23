305e713aa9ca0b1b465187942231904656233596
Montoya <MontoyaMontes@github.com>
2021-08-23 05:14:40 -0500

    Add contact form Add contact and PHP files to contact By:M

---
+++ php/mail.php
@@ -0,0 +1,20 @@
+<?php
+//get data from form  
+$name = $_POST['cont_name'];
+$email= $_POST['cont_email'];
+$subject = $_POST['cont_sub']
+$message= $_POST['cont_message'];
+
+$to = "montoyamontespi@ciencias.unam.mx";
+
+$subject = "[Mensaje de portafolio]" . $subject;
+$txt ="Nombre = ". $name . "\r\n  Email = " . $email . "\r\n Mensaje =" . $message;
+
+$headers = "From: noreply@personal-website.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
