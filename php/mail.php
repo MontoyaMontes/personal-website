57aa0a0c90b881c5d0121dfa70f39d42e98d6934
Montoya <MontoyaMontes@github.com>
2021-08-23 05:41:36 -0500

    Change html to send message

--- php/mail.php
+++ php/mail.php
@@ -5,15 +5,12 @@ $email= $_POST['cont_email'];
 $subject = $_POST['cont_sub']
 $message= $_POST['cont_message'];
 
-$to = "montoyamontespi@ciencias.unam.mx";
-
+$to = 'montoyamontespi@ciencias.unam.mx';
 $subject = "[Mensaje de portafolio]" . $subject;
 $txt ="Nombre = ". $name . "\r\n  Email = " . $email . "\r\n Mensaje =" . $message;
 
-$headers = "From: noreply@personal-website.com" . "\r\n" .
-"CC: somebodyelse@example.com";
 if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+    mail($to,$subject,$txt);
 }
 //redirect
 //header("Location:index.html");
